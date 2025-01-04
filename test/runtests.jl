325b132c96de620d384667241383cfe48b234c99
Joey Carpinelli <joey@loopy.codes>
2025-01-03 22:17:02 -0500

    Fixes test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,8 @@
 using Test
 using ExperimentalTagBot
 
-@test isempty(ExperimentalTagBot.untagged_versions("ExperimentalTagBot"))
+# TODO remove @test_throws when (if) this package is registered 
+@test_throws ErrorException begin
+    vs = ExperimentalTagBot.untagged_versions("ExperimentalTagBot")
+    @test isempty(vs)
+end
